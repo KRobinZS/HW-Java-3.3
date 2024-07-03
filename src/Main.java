e45a46d63cedeb8324f70f329296682c5d753202
Fasakhova <kvisti2009@mail.ru>
2024-07-03 19:31:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+int monthlyPayment = service.calculate(12);
+        System.out.println("Ежемесяный платеж="+monthlyPayment);
+    }
+}
